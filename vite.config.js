0b037e0cbafcf5be70f3d407552ae6ad35fced62
professionalvvr <professionalvvr@gmail.com>
2025-08-13 12:39:11 +0530

    commi

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
-    },
-  },
 })
